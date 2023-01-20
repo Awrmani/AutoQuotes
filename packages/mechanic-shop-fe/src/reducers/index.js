aaa963980001d64b9100ed285f7747786c014053
Denes Dolhay <denke@denkesys.com>
2023-01-20 10:29:58 -0500

    feat: Add token reducer

--- packages/mechanic-shop-fe/src/reducers/index.js
+++ packages/mechanic-shop-fe/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import lsStorageEngine from 'redux-persist/lib/storage';
 import formsReducer from '@autoquotes/libraries/src/reducers/formsReducer';
+import tokenReducer from '@autoquotes/libraries/src/reducers/tokenReducer';
 
 // Root reducer
 const rootReducer = combineReducers({
   forms: formsReducer,
+  token: tokenReducer,
 });
 
 const rootPersistConfig = {
@@ -13,7 +15,7 @@ const rootPersistConfig = {
   version: 1,
   keyPrefix: 'AutoQuotesShop.',
   key: 'root',
-  whitelist: [],
+  whitelist: ['token'],
   storage: lsStorageEngine,
 };
 
