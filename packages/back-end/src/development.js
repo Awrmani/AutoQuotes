5d5c8c9475d8544073f6ce0ec8e7f2511b77df6d
Denes Dolhay <denke@denkesys.com>
2023-01-27 19:02:18 -0500

    fix: Try to use new mongo memory server version

--- packages/back-end/src/development.js
+++ packages/back-end/src/development.js
@@ -1,11 +1,11 @@
-const mongoUnit = require('mongo-unit');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const runServer = require('./backend');
 
-mongoUnit
-  .start() // Spin up an in-memory mongoDB
-  .then(url => {
-    process.env.NODE_ENV = 'development';
-    process.env.MONGO_CONN_STR = url;
+// https://github.com/nodkz/mongodb-memory-server
+const mongod = new MongoMemoryServer({ binary: { version: '6.0.0' } });
+mongod.start().then(() => {
+  process.env.NODE_ENV = 'development';
+  process.env.MONGO_CONN_STR = mongod.getUri();
 
-    runServer();
-  });
+  runServer();
+});
