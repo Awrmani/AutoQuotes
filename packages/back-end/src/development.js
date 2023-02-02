18ffed3d65854cba0baf6930d5e7808617bbaa1c
Denes Dolhay <denke@denkesys.com>
2023-02-01 22:53:14 -0500

    improvement: CI run branches

--- packages/back-end/src/development.js
+++ packages/back-end/src/development.js
@@ -2,7 +2,7 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const runServer = require('./backend');
 
 // https://github.com/nodkz/mongodb-memory-server
-const mongod = new MongoMemoryServer({ binary: { version: '6.0.0' } });
+const mongod = new MongoMemoryServer({ binary: { version: '6.0.3' } });
 mongod.start().then(() => {
   process.env.NODE_ENV = 'development';
   process.env.MONGO_CONN_STR = mongod.getUri();
