3d412b561882abe65ad102adc7b7c39e346eece9
Denes Dolhay <denke@denkesys.com>
2023-02-11 17:04:42 -0500

    Shop schema, validation, seeding and RU operations

--- packages/back-end/src/apiRoutes/shop/index.js
+++ packages/back-end/src/apiRoutes/shop/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const postLogin = require('./postLogin');
 const getCurrentUser = require('./getCurrentUser');
+const shopGet = require('./shopGet');
+const shopUpdate = require('./shopUpdate');
 const ShopUser = require('../../resources/ShopUser');
 const { authenticatorFactory } = require('../../utils/authentication');
 
@@ -13,5 +15,9 @@ const router = express.Router();
 
 router.post('/login', postLogin);
 router.get(`/users/current`, authenticator, getCurrentUser);
+
+// Shop RU handlers
+router.get('/shop', shopGet);
+router.patch('/shop', authenticator, shopUpdate);
 
 module.exports = router;
---
+++ packages/libraries/src/utils/validation/openingHours.js
@@ -0,0 +1,67 @@
+const { pull } = require('lodash');
+
+const DAYS = {
+  MONDAY: 'monday',
+  TUESDAY: 'tuesday',
+  WEDNESDAY: 'wednesday',
+  THURSDAY: 'thursday',
+  FRIDAY: 'friday',
+  SATURDAY: 'saturday',
+  SUNDAY: 'sunday',
+};
+
+const openingHours = oh => {
+  if (typeof oh !== 'object') return 'must be an object';
+
+  // Verify if the correct keys are only present
+  if (pull(Object.values(DAYS), ...Object.keys(oh)).length)
+    return `Only the following keys are allowed: ${Object.values(DAYS).join(
+      ', '
+    )}`;
+
+  // Check each day
+  for (const day of Object.keys(oh)) {
+    const dayDescriptor = oh[day];
+    const keyCount = Object.keys(dayDescriptor).length;
+
+    if (keyCount === 0) {
+      // The day object is empty, check the next
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+
+    if (
+      keyCount !== 4 ||
+      typeof dayDescriptor.openHour !== 'number' ||
+      typeof dayDescriptor.openMinute !== 'number' ||
+      typeof dayDescriptor.closeHour !== 'number' ||
+      typeof dayDescriptor.closeMinute !== 'number'
+    ) {
+      return `exactly the "openHour", "openMinute", "closeHour", "closeMinute" attributes need to be present in each provided day. Error found in ${day}.`;
+    }
+
+    if (
+      // Make sure hours are between 0 and 24, minutes are between 0 and 59
+      dayDescriptor.openHour < 0 ||
+      dayDescriptor.openHour > 24 ||
+      dayDescriptor.openMinute < 0 ||
+      dayDescriptor.openMinute > 59 ||
+      dayDescriptor.closeHour < 0 ||
+      dayDescriptor.closeHour > 24 ||
+      dayDescriptor.closeMinute < 0 ||
+      dayDescriptor.closeMinute > 59 ||
+      // Make sure if hr is 24, then there is no minutes
+      (dayDescriptor.openHour === 24 && dayDescriptor.openMinute) ||
+      (dayDescriptor.closeHour === 24 && dayDescriptor.closeMinute) ||
+      // Make sure open is ahead of close
+      dayDescriptor.closeHour * 100 + dayDescriptor.closeMinute <=
+        dayDescriptor.open * 100 + dayDescriptor.openMinute
+    ) {
+      return `Incorrect time given in ${day}.`;
+    }
+  }
+
+  return undefined;
+};
+
+module.exports = { openingHours };
