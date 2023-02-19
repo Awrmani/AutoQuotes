6d4fbe32f7beba8f7592b068b98874b8b8553bf1
vanhoantran <vanhoantranca@gmail.com>
2023-02-19 09:12:10 -0500

    passing edit prop to the form

--- packages/mechanic-shop-fe/src/screens/EditServiceScreen.js
+++ packages/mechanic-shop-fe/src/screens/EditServiceScreen.js
@@ -51,7 +51,7 @@ const EditServiceScreen = () => {
       validation={validator}
       action={updateService}
     >
-      <EditServiceForm />
+      <EditServiceForm edit />
     </Form>
   );
 };
