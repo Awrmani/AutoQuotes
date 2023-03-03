7ceb3894e33540e3725263afdbc08c4d2172fefa
VanHoanTran <89094498+VanHoanTran@users.noreply.github.com>
2023-03-02 23:01:32 -0500

    setting up Routing for end-user front-end (#109)
    * setting up Routing for end-user front-end
    
    * adding components to pass the test
    
    * fixing routing for login and registration
    
    ---------
    
    Co-authored-by: vanhoantran <vanhoantranca@gmail.com>

---
+++ packages/end-user-fe/src/components/EndUserRegistration/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EndUserRegistration = () => {
+  return <div>registration</div>;
+};
+
+export default EndUserRegistration;
---
+++ packages/end-user-fe/src/screens/EndUserQuotingPageScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EndUserQuotingPage from '../components/EndUserQuotingPage';
+
+const EndUserQuotingPageScreen = () => {
+  return <EndUserQuotingPage />;
+};
+
+export default EndUserQuotingPageScreen;
