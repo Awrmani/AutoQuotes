3f8eab5e20a88d7f928fb4965cf3b73bb4bdd85a
vanhoantran <vanhoantranca@gmail.com>
2023-03-09 13:22:57 -0500

    adding vehicle selector

--- packages/end-user-fe/src/components/EndUserQuotingPage/index.js
+++ packages/end-user-fe/src/components/EndUserQuotingPage/index.js
@@ -1,7 +1,17 @@
+import { Box, Container, Divider, Paper } from '@mui/material';
 import React from 'react';
+import VehicleInfo from './VehicleInfo';
 
 const EndUserQuotingPage = () => {
-  return <div>End-user front-end</div>;
+  return (
+    <Container component={Paper}>
+      <Box sx={{ my: 2 }}>
+        <VehicleInfo />
+        <Divider sx={{ mt: 2 }} />
+      </Box>
+      <div>End-user front-end</div>
+    </Container>
+  );
 };
 
 export default EndUserQuotingPage;
--- packages/end-user-fe/src/screens/EndUserQuotingPageScreen.js
+++ packages/end-user-fe/src/screens/EndUserQuotingPageScreen.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Form } from '@autoquotes/common/src/components/Form';
+
 import EndUserQuotingPage from '../components/EndUserQuotingPage';
 
+const initialValues = {
+  make: '',
+  model: '',
+  year: '',
+  engine: '',
+  body: '',
+};
 const EndUserQuotingPageScreen = () => {
-  return <EndUserQuotingPage />;
+  return (
+    <Form
+      initialValues={initialValues}
+      validation={null}
+      action={null}
+      onSuccess={null}
+    >
+      <EndUserQuotingPage />
+    </Form>
+  );
 };
 
 export default EndUserQuotingPageScreen;
