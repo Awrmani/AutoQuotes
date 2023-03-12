4fe20893dfed6aa824a1bddf4c51a621330a70b4
Denes Dolhay <denke@denkesys.com>
2023-03-12 12:58:59 -0400

    improvement: Quote schema and seed

--- packages/back-end/src/apiRoutes/shop/appointmentGet.js
+++ packages/back-end/src/apiRoutes/shop/appointmentGet.js
@@ -1,6 +1,7 @@
 const Appointment = require('../../resources/Appointment');
 const EndUser = require('../../resources/EndUser');
 const Quote = require('../../resources/Quote');
+const VehicleType = require('../../resources/VehicleType');
 
 module.exports = async (req, res) => {
   const { id } = req.params;
@@ -12,9 +13,19 @@ module.exports = async (req, res) => {
   const customer = await new EndUser().loadBy(customerId);
   const quote = await new Quote().loadBy(quoteId);
 
+  let vehicleType;
+  try {
+    vehicleType = await new VehicleType().loadById(
+      quote.attributes.vehicleTypeId
+    );
+  } catch (e) {
+    // noop
+  }
+
   res.json({
     appointment: appointment.attributes,
     customer: customer.attributes,
     quote: quote.attributes,
+    vehicleType: vehicleType?.attributes,
   });
 };
