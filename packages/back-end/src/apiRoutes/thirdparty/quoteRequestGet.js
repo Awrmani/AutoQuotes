88c6733cefb111c25bb26d0e3a0df5679a45234a
dadolhay <92409606+dadolhay@users.noreply.github.com>
2023-04-05 08:44:45 -0400

    feat: Third-party API (#151)
    Co-authored-by: Denes Dolhay <denke@denkesys.com>

---
+++ packages/back-end/src/apiRoutes/thirdparty/quoteRequestGet.js
@@ -0,0 +1,38 @@
+const { omit } = require('lodash');
+const ThirdPartyOfferRequest = require('../../resources/ThirdPartyOfferRequest');
+const Quote = require('../../resources/Quote');
+const VehicleType = require('../../resources/VehicleType');
+const ServiceType = require('../../resources/ServiceType');
+
+module.exports = async (req, res) => {
+  const { supplierId, quoteId } = req.params;
+
+  const promises = (
+    await ThirdPartyOfferRequest.ThirdPartySupplierModel.find(
+      { quoteId, supplierIds: supplierId },
+      ['_id']
+    ).exec()
+  ).map(({ id }) => new ThirdPartyOfferRequest().loadById(id.toString()));
+
+  const offerRequestPromises = (await Promise.all(promises)).map(async obj => {
+    const quote = await new Quote().loadById(obj.attributes.quoteId);
+
+    const vehicleType = await new VehicleType().loadById(
+      quote.attributes.vehicleTypeId
+    );
+
+    const serviceType = await new ServiceType().loadById(
+      obj.attributes.serviceTypeId
+    );
+
+    return {
+      ...omit(obj.attributes, ['supplierIds']),
+      vehicleType,
+      serviceType,
+    };
+  });
+
+  const offerRequests = await Promise.all(offerRequestPromises);
+
+  return res.json(offerRequests);
+};
