57a48e55c42b5e49d2d8b35c3eed5e6bf5c2f690
vanhoantran <vanhoantranca@gmail.com>
2023-04-20 12:30:28 -0400

    add some tests

--- packages/third-party-fe-e2e/cypress/e2e/demo.cy.js
+++ packages/third-party-fe-e2e/cypress/e2e/demo.cy.js
@@ -1,6 +1,30 @@
 describe('Demo', () => {
-  it('should display Third party FE', () => {
+
+  it(`should display car's info`, () => {
     cy.visit('/suppliers/firstSupplie/quotes/customQuote1');
-    cy.getByText('MCU 3 computer').should('exist');
+    cy.contains('Tesla 2022 Model S Plaid liftback');
+  });
+
+  it('should show parts requested', () => {
+    cy.visit('/suppliers/firstSupplie/quotes/customQuote1');
+    cy.contains('MCU 3 computer');
+    cy.contains('AP computer v3');
+    cy.contains('Scroll compressor');
+  });
+
+  it('should allow to send an offer', () => {
+    cy.visit('/suppliers/firstSupplie/quotes/customQuote1');
+    cy.getByText('MCU 3 computer').click();
+    // Fill in user creds from seed
+    cy.findByLabelText('Type').parent().click();
+    cy.getByTestId('select-option-OE').click();
+    cy.findByLabelText('Price *').type('325');
+    cy.findByLabelText('Warranty in months').type('12');
+    cy.findByLabelText('Manufacturer').type('Tesla');
+    cy.findByLabelText('Description').type(
+      `The improved processor that power car's infotainment system and the new graphics processor`
+    );
+    cy.getByTestId('submitButton').click();
+    cy.getByText('Offer sent!').should('exist');
   });
 });
